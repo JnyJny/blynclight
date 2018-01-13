80250e81679e4eaf70b821281efbe51791ba576f
Erik O'Shaughnessy <erik.oshaughnessy@gmail.com>
2018-01-13 17:19:07 -0600

    on() function mutated to on property

--- src/blynclight/__init__.py
+++ src/blynclight/__init__.py
@@ -5,9 +5,10 @@ devices.
 
 > from blynclight import BlyncLightControl
 > b = BlyncLightControl.getLight(0)
-> b.color = (255, 0, 0)
-> b.on()
-> b.off()
+> b.color = (0, 255, 0)                         # set color to green
+> b.on = True                                   # turn light on
+> b.color = (255, 0, 0)                         # set color to red
+> b.on = False                                  # turn light off
 
 '''
 
