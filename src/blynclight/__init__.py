945be1723c0c5ce7534e5728cfa0fb1f4bb83ce4
Erik O'Shaughnessy <erik.oshaughnessy@gmail.com>
2018-01-20 14:17:01 -0600

    constants reorganized and added proxy support

--- src/blynclight/__init__.py
+++ src/blynclight/__init__.py
@@ -1,7 +1,9 @@
 '''python bindings for Embrava BlyncLight devices.
 
-This module provides python bindings for Embrava BlyncLight family of
-devices.
+This module provides python bindings for interacting with the Embrava
+BlyncLight family of devices.
+
+Control lights directly:
 
 > from blynclight import BlyncLightControl
 > b = BlyncLightControl.getLight(0)
@@ -10,21 +12,38 @@ devices.
 > b.color = (255, 0, 0)                         # set color to red
 > b.on = False                                  # turn light off
 
+Control lights via http API:
+
+> from blynclight import BlyncLightProxy
+> proxy = BlyncLightProxy('http://yourhost:port')
+> available = proxy.status()
+> proxy.on(0)
+> proxy.red(0, 255)
+> proxy.green(0, 0)
+> proxy.blue(0, 255)
+> proxy.color(0, (255,255,255))
+> proxy.off(0)
+
+
 '''
 
-from .blynclight import BlyncLightControl, BlyncLight, DeviceInfo
-from .color import Color, ColorToRGB, HexToRGB
-from .devicetype import DeviceType
-from .flashspeed import FlashSpeed
+from .blynclight import BlyncLightControl, BlyncLight
+from .color import ColorToRGB, HexToRGB, RGBToHex
+from .constants import (Color, DeviceType, DeviceInfo, FlashSpeed)
+from .proxy import Proxy as BlyncLightProxy
 
-__all__ = [ 'BlyncLight',
-            'BlyncLightControl',
-            'Color',
-            'ColorToRGB',
-            'DeviceInfo',
-            'DeviceType',
-            'FlashSpeed',
-            'HexToRGB']
+__all__ = [
+    'BlyncLight',
+    'BlyncLightControl',
+    'ColorToRGB',
+    'HexToRGB',
+    'RGBToHex',
+    'Color',
+    'DeviceType',
+    'DeviceInfo',
+    'FlashSpeed',
+    'BlyncLightProxy',
+]
 
 
             
