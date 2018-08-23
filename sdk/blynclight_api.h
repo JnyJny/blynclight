edce99961a37f15e2429f1802a08b88936205125
JnyJny <erik.oshaughnessy@gmail.com>
2018-08-23 10:22:21 -0500

    Refactored Fascade API
    I wanted to reduce the number of C files need to support the
    python module, so I found a better way to implement the former
    patch API. So added blynclight_api.[ch] and plastered over the
    Embrava APIs with a new API that is simpler to me. The goal was
    to reduce the number of functions exposed to the python module.

---
+++ sdk/blynclight_api.h
@@ -0,0 +1,54 @@
+/* blynclight_api.h 
+ */
+#ifndef BLYNCLIGHT_API_H
+#define BLYNCLIGHT_API_H
+
+#ifdef __linux
+#include "constants.h"
+#include "hidapi.h"
+#include "embravaapi.h"
+#define MAXIMUM_DEVICES MAX_DEVICES_SUPPORTED
+struct DeviceInfo
+{
+  unsigned char byDeviceType;
+} asDeviceInfo[MAXIMUM_DEVICES];
+
+#define RETVAL(V) V
+#define TurnOffLight ResetLight
+#define ReleaseDevices(X) CloseDevices(X)
+#define DEVINFO_ARRAY aosDeviceInfo
+#endif
+
+#ifdef __APPLE__
+typedef unsigned char Byte;
+typedef unsigned char byte;
+#include "Constants.h"
+#include "blynclightcontrol.h"
+#define RETVAL(V) ((V)==0)?1:0
+#define ReleaseDevices(X) ReleaseDevices()
+#define InitBlycDevices(IPTR, ARRAY) FindDevices(IPTR)
+#define DEVINFO_ARRAY asDeviceInfo
+#endif
+
+int  init_blynclights(void);
+void fini_blynclights(int ndevices);
+int  refresh_blynclights(void);
+
+byte device_type(byte index);
+
+int  light_on(byte index, byte r, byte g, byte b);
+int  light_off(byte index);
+
+int  bright(byte index, byte mode);
+
+int  flash(byte index, byte mode);
+int  flash_speed(byte index, byte speed);
+
+int  music(byte index, byte mode);
+int  music_repeat(byte index, byte mode);
+int  music_volume(byte index, byte volume);
+int  music_select(byte index, byte music);
+
+int  mute(byte index, byte mode);
+
+#endif	/* BLYNCLIGHT_API_H */
