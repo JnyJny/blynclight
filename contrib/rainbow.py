642f5495d5798bce48bdbd446308ff13ae3796b0
JnyJny <erik.oshaughnessy@gmail.com>
2018-08-30 22:20:35 -0500

    s/rgb/rbg/ to match device implementation order

--- contrib/rainbow.py
+++ contrib/rainbow.py
@@ -11,15 +11,15 @@ from itertools import cycle
 
 
 def Spectrum(steps=64, frequency=None, phase=None, center=128, width=127):
-    '''generator function that returns 'steps' rgb tuples.
+    '''generator function that returns 'steps' rbg tuples.
 
         steps: optional integer, default=64
-    frequency: optional 3-tuple for rgb frequency, default=(.3,.3,.3)
-        phase: optional 3-tuple for rgb phase, default=(0,2,4)
+    frequency: optional 3-tuple for rbg frequency, default=(.3,.3,.3)
+        phase: optional 3-tuple for rbg phase, default=(0,2,4)
        center: optional integer, default=128
         width: optional integer, default=127
 
-    Returns (r, g, b) where each member is a value between 0 and 255.
+    Returns (r, b, g) where each member is a value between 0 and 255.
     '''
 
     frequency = frequency or (.3, .3, .3)
@@ -27,9 +27,9 @@ def Spectrum(steps=64, frequency=None, phase=None, center=128, width=127):
 
     for i in range(steps):
         r = int((math.sin(frequency[0] * i + phase[0]) * width) + center)
-        g = int((math.sin(frequency[1] * i + phase[1]) * width) + center)
         b = int((math.sin(frequency[2] * i + phase[2]) * width) + center)
-        yield (r, g, b)
+        g = int((math.sin(frequency[1] * i + phase[1]) * width) + center)
+        yield (r, b, g)
 
 
 if __name__ == '__main__':
@@ -51,7 +51,7 @@ if __name__ == '__main__':
     colors = [rgb for rgb in Spectrum(steps=255)]
 
     try:
-        b = NewBlyncLight.available_lights()[args.light_id]
+        b = BlyncLight.available_lights()[args.light_id]
     except IndexError:
         print(f'light {args.light_id} unavailable')
         exit(-1)
