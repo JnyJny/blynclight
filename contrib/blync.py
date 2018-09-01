0f1168e8c0975971aac9dcc03125cd8cfd588f33
JnyJny <erik.oshaughnessy@gmail.com>
2018-09-01 11:10:48 -0500

    Added blync.py to contrib for debugging
    The blync.py script in contrib was written to debug state transitions
    and will probably make it's way into the module.

---
+++ contrib/blync.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+from blynclight import BlyncLight
+from argparse import ArgumentParser
+
+
+def main():
+
+    parser = ArgumentParser()
+    parser.add_argument('-l', '--light', type=int, default=-1)
+    parser.add_argument('-r', '--red', type=int, default=0)
+    parser.add_argument('-b', '--blue', type=int, default=0)
+    parser.add_argument('-g', '--green', type=int, default=0)
+    parser.add_argument('-o', '--on', action='store_true', default=False)
+    parser.add_argument('-d', '--dim', action='store_true', default=False)
+    parser.add_argument('-f', '--flash', action='store_true', default=False)
+    parser.add_argument('-s', '--speed', type=int, default=0)
+    parser.add_argument('-m', '--music', type=int, default=0)
+    parser.add_argument('-n', '--dryrun', action='store_true', default=False)
+    parser.add_argument('-p', '--play', action='store_true', default=False)
+    parser.add_argument('-R', '--repeat', action='store_true', default=False)
+    parser.add_argument('-M', '--mute', action='store_true', default=False)
+    parser.add_argument('-v', '--volume', type=int, default=0)
+    parser.add_argument('-V', '--verbose', action='store_true', default=False)
+    
+    args = parser.parse_args()
+
+    if args.light >= 0:
+        light = BlyncLight.available_lights()[args.light]
+    else:
+        light = BlyncLight.first_light()
+
+    light.immediate = False
+    light.red = args.red
+    light.blue = args.blue
+    light.green = args.green
+    light.on = args.on
+    light.dim = args.dim
+    light.flash = args.flash
+    light.speed = args.speed
+    light.music = args.music
+    light.play = args.play
+    light.repeat = args.repeat
+    light.mute = args.mute
+    light.volume = args.volume
+        
+    if not args.dryrun:
+        light.update_device()
+        
+    if args.verbose or args.dryrun:
+        print(light)
+
+
+if __name__ == '__main__':
+    main()
+
