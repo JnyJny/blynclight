16369a70684476ea974c5a1fa79cf4fdf0bf4d0e
JnyJny <erik.oshaughnessy@gmail.com>
2018-08-31 23:22:13 -0500

    Test BlyncLight init and bit field set/get

--- tests/test_blynclight_dev.py
+++ tests/test_blynclight_dev.py
@@ -2,121 +2,111 @@
 '''
 
 import pytest
-from blynclight import (BlyncLight_API,
+from blynclight import (BlyncLight,
                         DeviceType,
                         MusicVolume,
                         FlashSpeed)
 
 
 @pytest.fixture
-def blynclight_api():
-    return BlyncLight_API()
-
-
-@pytest.fixture
 def blynclight():
-    return BlyncLight_API.first_light()
+    return BlyncLight.first_light()
 
 
-@pytest.fixture
-def DEV():
-    return 0
+def test_blynclight_classmethods():
+
+    assert len(BlyncLight.available_lights()) != 0
+    assert BlyncLight.first_light()
 
 
-def test_blynclight_api_init(blynclight_api):
-    '''
-    '''
-    assert blynclight_api._instance == blynclight_api
+def test_blynclight_init(blynclight):
+
+    assert blynclight.report == 0
+    assert blynclight.red == 0
+    assert blynclight.blue == 0
+    assert blynclight.green == 0
+    assert blynclight.off == 1
+    assert blynclight.dim == 0
+    assert blynclight.flash == 0
+    assert blynclight.speed == 0
+    assert blynclight.pad0 == 0
+    assert blynclight.mute == 0
+    assert blynclight.music == 0
+    assert blynclight.start == 0
+    assert blynclight.repeat == 0
+    assert blynclight.pad1 == 0
+    assert blynclight.volume == 0
+    assert blynclight.pad2 == 0
+    assert blynclight.eom == 0xffff
+    assert blynclight.vendor_id == 0x2c0d
+    assert blynclight.product_id > 0
+    assert blynclight._handle
+    assert blynclight.immediate
 
 
-def test_blynclight_api_methods(blynclight_api):
-    '''
-    '''
-    for func in blynclight_api._funcs.keys():
-        assert getattr(blynclight_api, func)
+def test_blynclight_bitfields(blynclight):
 
-
-def test_blynclight_api_dev_nlights(blynclight_api):
-    '''
-    '''
-    assert blynclight_api.nlights == 1
-
-
-def test_blynclight_api_dev_unique_device(blynclight_api):
-    '''
-    '''
-    assert blynclight_api.unique_device_id(DEV) != 0
-
-
-def test_blynclight_api_dev_device_type(blynclight_api):
-    '''
-    '''
-    assert blynclight_api.device_type(DEV) == DeviceType.INVALID
-
-
-def test_blynclight_api_dev_light_on(blynclight_api):
-    '''
-    '''
-    assert blynclight_api.light_on(DEV, 0, 0, 0) == 1
-
-
-def test_blynclight_api_dev_light_off(blynclight_api):
-    '''
-    '''
-    assert blynclight_api.light_on(DEV, 0, 0, 0) == 1
-
-
-def test_blynclight_api_dev_bright(blynclight_api):
-    '''
-    '''
-    assert blynclight_api.bright(DEV, 0) == 1
-    assert blynclight_api.bright(DEV, 1) == 1
-
-
-def test_blynclight_api_dev_flash(blynclight_api):
-    '''
-    '''
-    assert blynclight_api.flash(DEV, 0) == 1
-    assert blynclight_api.flash(DEV, 1) == 1
-
-
-def test_blynclight_api_dev_flash_speed(blynclight_api):
-    '''
-    '''
-    for speed in range(FlashSpeed):
-        assert blynclight_api.flash_speed(DEV, speed) == 1
-
-
-def test_blynclight_api_dev_music(blynclight_api):
-    '''
-    '''
-    assert blynclight_api.music(DEV, 0) == 1
-    assert blynclight_api.music(DEV, 1) == 1
-
-
-def test_blynclight_api_dev_music_repeat(blynclight_api):
-    '''
-    '''
-    assert blynclight_api.music_repeat(DEV, 0) == 1
-    assert blynclight_api.music_repeat(DEV, 1) == 1
-
-
-def test_blynclight_api_dev_music_volume(blynclight_api):
-    '''
-    '''
-    for volume in range(MusicVolume):
-        assert blynclight_api.music_volume(DEV, volume.value) == 1
-
-
-def test_blynclight_api_dev_music_select(blynclight_api):
-    '''
-    '''
-    for m in range(10):
-        assert blynclight_api.music_select(DEV, m) == 1
-
-
-def test_blynclight_api_dev_mute(blynclight_api):
-    '''
-    '''
-    assert blynclight_api.mute(DEV, 0) == 1
-    assert blynclight_api.mute(DEV, 1) == 1
+    blynclight.immediate = False
+    assert not blynclight.immediate
+    blynclight.report = 1
+    assert blynclight.report == 0
+    blynclight.red = 0x11
+    assert blynclight.red == 0x11
+    blynclight.blue = 0x22
+    assert blynclight.blue == 0x22
+    blynclight.green = 0x33
+    assert blynclight.green == 0x33
+    blynclight.off = 0
+    assert blynclight.off == 0
+    blynclight.off = 1
+    assert blynclight.off == 1
+    blynclight.on = 0
+    assert blynclight.off == 1
+    blynclight.on = 1
+    assert blynclight.off == 0
+    blynclight.dim = 1
+    assert blynclight.dim == 1
+    blynclight.dim = 0
+    assert blynclight.dim == 0
+    blynclight.bright = 0
+    assert blynclight.dim == 1
+    blynclight.bright = 1
+    assert blynclight.dim == 0
+    blynclight.flash = 1
+    assert blynclight.flash == 1
+    blynclight.flash = 0
+    assert blynclight.flash == 0
+    for i in range(0, 3):
+        blynclight.speed = 1 << i
+        assert blynclight.speed == 1 << i
+    blynclight.pad0 = 1
+    assert blynclight.pad0 == 0
+    blynclight.mute = 1
+    assert blynclight.mute == 1
+    blynclight.mute = 0
+    assert blynclight.mute == 0
+    for i in range(0, 11):
+        blynclight.music = i
+        assert blynclight.music == i
+    blynclight.start = 1
+    assert blynclight.start == 1
+    blynclight.start = 0
+    assert blynclight.start == 0
+    blynclight.repeat = 1
+    assert blynclight.repeat == 1
+    blynclight.repeat = 0
+    assert blynclight.repeat == 0
+    blynclight.pad1 = 1
+    assert blynclight.pad1 == 0
+    for i in range(0, 11):
+        blynclight.volume = i
+        assert blynclight.volume == i
+    blynclight.pad2 = 1
+    assert blynclight.pad2 == 0
+    blynclight.eom = 0
+    assert blynclight.eom == 0xffff
+    assert blynclight.vendor_id == 0x2c0d
+    assert blynclight.product_id > 0
+    assert blynclight._handle
+    blynclight.immediate = True
+    assert blynclight.immediate
