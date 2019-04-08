873d28f9f0e140a6c29a68ca25f02c18512f0f1d
JnyJny <erik.oshaughnessy@gmail.com>
2019-04-08 12:35:13 -0500

    Updated formatting with black

--- contrib/fli.py
+++ contrib/fli.py
@@ -9,42 +9,54 @@ from collections import deque
 from time import sleep
 import click
 
+
 def rotatable_color(intensity: str):
-    '''
+    """
     :param intensity: string hexadecimal number
     :return: collections.deque of integers
-    '''
-    I = (0xff & int(intensity, 16)) >> 0
+    """
+    I = (0xFF & int(intensity, 16)) >> 0
     return deque([I, 0, 0])
 
 
 @click.command()
-@click.option('-l', '--light-id', default=0, help='Integer light identfier.')
-@click.option('-i', '--interval', default=0.1, help='Float interval in seconds to flash.')
-@click.option('-c', '--count', default=-1, help='Integer count to flash impressively.')
-@click.option('-I', '--intensity', default='0xff', help='8-bit color intensity.' )
-@click.option('-a', '--available', is_flag=True, help='List available BlyncLights and exit.')
+@click.option("-l", "--light-id", default=0, help="Integer light identfier.")
+@click.option(
+    "-i", "--interval", default=0.1, help="Float interval in seconds to flash."
+)
+@click.option(
+    "-c", "--count", default=-1, help="Integer count to flash impressively."
+)
+@click.option(
+    "-I", "--intensity", default="0xff", help="8-bit color intensity."
+)
+@click.option(
+    "-a",
+    "--available",
+    is_flag=True,
+    help="List available BlyncLights and exit.",
+)
 def flash_light_impressively(light_id, interval, count, intensity, available):
-    '''FLI - Flash Light Impressively
+    """FLI - Flash Light Impressively
 
     Cycles the light identified by light_id thru red, green and blue
     with the given intensity count times, pausing for interval
     seconds. It can be stupid annoying. Apologize to your cow-orkers
     for me.
 
-    '''
+    """
 
     if available:
         BlyncLight.report_available()
         return
-    
+
     try:
         light = BlyncLight.get_light(light_id)
     except Exception as e:
         return
 
     light.on = 1
-    
+
     color = rotatable_color(intensity)
 
     try:
@@ -60,4 +72,3 @@ def flash_light_impressively(light_id, interval, count, intensity, available):
 if __name__ == "__main__":
 
     flash_light_impressively()
-
