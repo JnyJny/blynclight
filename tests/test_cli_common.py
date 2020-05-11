0ca51a94f25eab9aeb5e9939e37bac9d95cb6566
JnyJny <erik.oshaughnessy@gmail.com>
2020-05-11 18:23:39 -0500

    Updated cli_common tests to work on just blync CLI

--- tests/test_cli_common.py
+++ tests/test_cli_common.py
@@ -9,22 +9,19 @@ from blynclight.__main__ import blync_cli, fli_cli, rainbow_cli, throbber_cli
 cli_s = [blync_cli, fli_cli, rainbow_cli, throbber_cli]
 
 
-@pytest.mark.parametrize("cli", cli_s)
-def test_common_cli_version(cli, Runner):
+def test_common_cli_version(Runner):
     result = Runner.invoke(blync_cli, ["--version"])
     assert result.exit_code == 0
     assert __version__ in result.output
 
 
-@pytest.mark.parametrize("cli", cli_s)
-def test_common_cli_help(cli, Runner):
+def test_common_cli_help(Runner):
     result = Runner.invoke(blync_cli, ["--help"])
     assert result.exit_code == 0
     assert "Usage" in result.output
 
 
-@pytest.mark.parametrize("cli", cli_s)
-def test_common_cli_list_available_lights(cli, Runner):
+def test_common_cli_list_available_lights(Runner):
     result = Runner.invoke(blync_cli, ["--list-available"])
     assert result.exit_code == 0
-    assert "KEY:ID:VALUE" in result.output
+    assert "Number of available lights:" in result.output
