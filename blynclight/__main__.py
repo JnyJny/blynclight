5593f4023e210ffbb8685ba899827d82b99c3878
JnyJny <erik.oshaughnessy@gmail.com>
2020-06-22 22:10:40 -0500

    Updated docstring for hidapi information

--- blynclight/__main__.py
+++ blynclight/__main__.py
@@ -180,9 +180,9 @@ def blync_callback(
     $ python3 -m pip install git+https://github.com/JnyJny/blynclight.git # latest
     ```
 
-    This module depends on [hidapi](https://github.com/libusb/hidapi), which supports
-    Windows, Linux, FreeBSD and MacOS. Check your favorite package managers before
-    attempting to download and build the library from source.
+    This module depends on
+    [hidapi](https://github.com/trezor/cython-hidapi), which supports
+    Windows, Linux, FreeBSD and MacOS via a Cython module.
     """
 
     try:
