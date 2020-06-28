88158e0e70017133a95024031ddb8ad817e02b2a
JnyJny <erik.oshaughnessy@gmail.com>
2020-06-27 19:13:40 -0500

    Improving docstrings

--- blynclight/bitvector.py
+++ blynclight/bitvector.py
@@ -10,13 +10,30 @@ from loguru import logger
 
 
 class BitField:
-    """Data Descriptor for naming fields in a BitVector.
+    """Data Descriptor for accessing named fields in a BitVector.
+
+    class MyBV(BitVector):
+        ...
+        byte0 = BitField(0, 8)
+        byte1 = BitField(8, 8)
+        bit16 = BitField(16)
+        bit17 = BitField(17)
+        pad0  = BitField(18, 4)
+        byte3 = BitField(32, 8)
+    
+    > mybv = MyBV()
+    > mybv.byte0 = 0xff
+    > mybv.bit17 = 1
+    > mybv.byte3 = 0x55
+    > mybv
+    MyBV(value=0x5502ff, length=128)
+
     """
 
     def __init__(self, offset: int, width: int = 1):
         """
         :param offset: int
-        :param width: int [defaults to 1]
+        :param width: int
         """
         self.field = slice(offset, offset + width)
 
@@ -37,52 +54,61 @@ class BitField:
 
 @functools.total_ordering
 class BitVector:
+    """A Bit Vector is a list of bits in packed (integer)
+    format that can be accessed by indexing into the vector
+    or using a slice (via conventional square brackets 
+    notation). 
+
+    """
+
     def __init__(self, value: int = 0, size: int = 128):
         self.MAX = (1 << size) - 1
         self.value: int = value & self.MAX
 
-    def _getb(self, bit: int) -> int:
+    def _getb(self, offset: int) -> int:
+        """Retrieves the bit value at offset."""
+        return (self.value >> offset) & 0x1
 
-        return (self.value >> bit) & 0x1
+    def _setb(self, offset: int) -> None:
+        """Sets the bit value at offset."""
+        self.value |= (1 << offset) & self.MAX
 
-    def _setb(self, bit: int) -> None:
+    def _clrb(self, offset: int) -> None:
+        """Clears the bit value at offset."""
+        self.value &= ~(1 << offset)
 
-        self.value |= (1 << bit) & self.MAX
-
-    def _clrb(self, bit: int) -> None:
-
-        self.value &= ~(1 << bit)
-
-    def _setval(self, bit: int, value: int):
+    def _setval(self, offset: int, value: int):
         if value:
-            self._setb(bit)
+            self._setb(offset)
         else:
-            self._clrb(bit)
+            self._clrb(offset)
 
-    def toggle(self, bit: int) -> int:
-        """Toggle the n-th `bit` in the vector and return it's previous value.
+    def toggle(self, offset: int) -> int:
+        """Toggle the bit at offset in the vector and return the previous value.
         """
-        self.value ^= (1 << bit) & self.MAX
-        return not self._getb(bit)
+        prev = self._getb(offset)
+        self.value ^= (1 << offset) & self.MAX
+        return prev
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}(value={self!s}, size={len(self)})"
 
     def __str__(self) -> str:
-        hexstr = "0x{{:0{0}x}}".format(len(self) // 4)
+        nibbles = (len(self) // 4) + 1 if (len(self) % 4) else 0
+        hexstr = "0x{{:0{0}x}}".format(nibbles)
         return hexstr.format(self.value)
 
     def __len__(self) -> int:
-        """Length of the bit vector in bits."""
+        """Length of the vector in bits."""
         try:
             return self._len
         except AttributeError:
             pass
-        self._len = bin(self.MAX).count("1")
+        self._len = self.MAX.bit_length()
         return self._len
 
     def __getitem__(self, key: Union[int, slice]) -> int:
-
+        """Given a key, retrieve a bit or bitfield."""
         try:
             return self._getb(key & (len(self) - 1))
         except TypeError:
@@ -96,7 +122,17 @@ class BitVector:
         return value
 
     def __setitem__(self, key: Union[int, slice], value: Union[int, bool]) -> None:
-        """
+        """Given a key, set a bit or bitfield to the supplied value.
+
+        If value is True or False and the key is a slice describing a
+        bit field, each bit in the field takes on the value.  Otherwise,
+        the value is left shifted and the lsb is added to the next offset
+        in the bit field.
+
+        > b[:8] = True   # results in b0 - b7 == 1
+        > b[:7] = 0x1    # results in b0 == 1 and the rest of the bits == 0
+        
+        The difference is subtle and perhaps should not be considered a feature.
         """
         try:
             if key < 0:
@@ -121,7 +157,18 @@ class BitVector:
             raise ValueError("Expected int or slice key") from None
 
     def __binary_op(self, other, func, return_obj: bool = False, reverse: bool = False):
+        """Calls the supplied function `func` with self and other.
 
+        If the user sets return_obj to True, a new BitVector initialized with the
+        results of `func` is returned.  If `reverse` is True, the order of self and other
+        is reversed in the call to `func`.
+
+        :param other: Union[int, BitVector]
+        :param func: callable from operator
+        :param return_obj: bool
+        :param reverse: bool
+        :return: Union[int, bool, BitVector]
+        """
         a, b = (self, other) if not reverse else (other, self)
 
         try:
@@ -138,13 +185,29 @@ class BitVector:
         return retval
 
     def __unary_op(self, func, return_obj: bool = False):
+        """Calls the supplied function `func` with self and returns the result.
+
+        If return_obj is True, the return value is a BitVector initialized from
+        the results of `func`. 
+
+        :param func: callable from operator 
+        :param return_obj: bool
+        :return: Union[int, BitVector]
+        """
 
         retval = func(self.value) & self.MAX
         if return_obj:
             retval = self.__class__(retval)
         return retval
 
-    def __inplace_op(self, other, func):
+    def __inplace_op(self, other, func) -> object:
+        """Calls the supplied binary function `func` with self and other
+        and updates self with the results. 
+
+        :param other:  Union[int, BitVector]
+        :param func: Callable from operator
+        :return: self
+        """
         try:
             self.value = func(self.value, other.value) & self.MAX
         except AttributeError:
@@ -153,13 +216,16 @@ class BitVector:
         return self
 
     def bin(self) -> str:
+        """Binary string representation of BitVector."""
         return bin(self.value)
 
     def hex(self) -> str:
+        """Hexadecimal string representation of BitVector."""
         return hex(self.value)
 
     @property
     def bytes(self) -> bytes:
+        """Bytes representation of BitVector."""
         n = len(self) // 8 + (1 if len(self) % 8 else 0)
         return self.value.to_bytes(n, "big")
 
